e18bc73a470f1754b7d12a1560ef8d46721163d4
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-02-24 13:03:48 +0100

    Update

--- Exercise7/Program.cs
+++ Exercise7/Program.cs
@@ -2,48 +2,51 @@
 {
     static void Main()
     {
-        int maxSum = 0,tempSum=0;
-        Console.Write("Please enter the length of the array: ");
+        Console.Write("Please enter the size of your array: ");
         int n = int.Parse(Console.ReadLine());
-        Console.Write("Please enter the length of the subset you would like to form: ");
-        int k = int.Parse(Console.ReadLine()); 
-        Console.WriteLine("Please enter an array of integers: ");
-        int[] ints = new int[n];
-        int count = n/k;
-        int l = 0,m=0,p=0;
+        Console.Write("Please enter the size of the subset you'd like to form: ");
+        int k = int.Parse(Console.ReadLine());
+
+        int[] myArray = new int[n];
+        int[] tempset = new int[k];
+        int[] subset = new int[k];
+        int maxSum = 0, sum = 0, limit = n - k, inc = 0, inc2 = k;
 
         for (int i = 0; i < n; i++)
         {
-            ints[i] = int.Parse(Console.ReadLine());
+            Console.Write("Please enter element {0}: ", i + 1);
+            myArray[i] = int.Parse(Console.ReadLine());
         }
 
-        int[] subset = new int[k];
-        int[] tempset = new int[k];
-
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i<n ; i++)
         {
-            for(int j = m; j < k; j++)
+            int tempsum = 0;
+            for (int j = inc, l = 0; l < inc2; j++, l++)
             {
-                tempSum += ints[j];
-                tempset[p] = ints[j];
-                if(tempSum > maxSum)
-                {
-                   maxSum = tempSum;
-                    subset = tempset;
-                }
-                p++;
+
+                if (j == n) { break; }
+                tempset[l] = myArray[j];
+                tempsum += subset[l];
+                sum = tempsum;
             }
-            m++;
+
             k++;
+            inc++;
+
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                subset = tempset;
+            }
+
         }
 
-        Console.Write("The subset with the greatest sum is ");
+        Console.WriteLine("The maximum sum is: " + maxSum);
+        Console.Write("The elements involved are: ");
 
-        for(int i=0; i<k; i++)
+        for (int i = 0; i < inc2; i++)
         {
-            Console.Write(" "+subset[i]);
+            Console.Write(subset[i] + " ");
         }
-
-        Console.Write(" with a sum of "+maxSum);
     }
 }
