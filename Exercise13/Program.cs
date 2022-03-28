fafdefb0988a164e1e526f81788326d409fbadb8
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-03-28 12:34:18 +0100

    Final Update

--- Exercise13/Program.cs
+++ Exercise13/Program.cs
@@ -1,7 +1,79 @@
 ﻿class Exercise13
 {
+    static int[,] ints;
+    static int n,m;
     static void Main()
     {
+        bool errorCatch = false;
+        Console.Write("Please enter the length of the columns of your array: ");
+        while (errorCatch == false)
+        {
+            errorCatch = int.TryParse(Console.ReadLine(), out n);
+        }
 
+        errorCatch = false;
+        Console.Write("Please enter the length of the rows of your array: ");
+        while (errorCatch == false)
+        {
+            errorCatch = int.TryParse(Console.ReadLine(), out m);
+        }
+
+        ints = new int[n,m];
+        errorCatch = false;
+        int lim = 0;
+
+        Console.WriteLine("\nPlease enter the integers to be stored in the array: ");
+        for(int i = 0; i < n; i++)
+        {
+            for(int j=0; j < m; )
+            {
+                Console.Write("\nEnter the value to be stored in [{0},{1}]: ", i, j);
+                errorCatch= int.TryParse(Console.ReadLine(), out ints[i,j]);
+                if (errorCatch) { j++; }
+                else { Console.WriteLine("\nInvalid value entered, Please try again\n"); }
+            }
+        }
+
+        if (n >= 3 && m >= 3)
+        {
+            maximalPlatform(ints, n, m);
+        }
+        else { Console.WriteLine("\nThe array does not match the requirements. Try again later"); }
+        
+    }
+
+    static void maximalPlatform(int[,]ints,int n,int m)
+    {
+        int maxSum = 0,maxRow=0,maxCol=0;
+
+        for(int row = 0; row < n - 2; row++)
+        {
+            for(int col = 0; col < m - 2; col++)
+            {
+                int sum=ints[row,col]+ ints[row, col+1]+ ints[row, col+2]+ ints[row+1, col] + ints[row + 1, col+1] + ints[row + 1, col+2]+ 
+                    ints[row+2, col] + ints[row + 2, col+1] + ints[row + 2, col+2];
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxRow = row;
+                    maxCol = col;
+                }
+            }
+        }
+        printArray(maxSum, maxCol, maxRow);
+    }
+
+    static void printArray(int maxSum, int maxCol, int maxRow)
+    {
+        Console.WriteLine("\nThe 3 x 3 platform with a maximal sum of {0} is :",maxSum);
+        for(int i=maxRow; i < maxRow+3; i++)
+        {
+            for(int j=maxCol;j < maxCol+3; j++)
+            {
+                Console.Write(ints[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
     }
 }
---
+++ Exercise15/Program.cs
@@ -0,0 +1,53 @@
+﻿class Exercise15
+{
+    static void Main()
+    {
+
+        Console.Write("Please enter a word and end it with a full stop or period: ");
+        int count = 0;
+        char[] letters = new char[25];
+        do
+        {
+            var x=Console.Read();
+           letters[count] = Convert.ToChar(x);
+            if(letters[count] == '.') { break; }
+            count++;
+        }
+
+        while (count<25);
+
+        LetterIndices(letters,count);
+    }
+
+    static void LetterIndices(char[] letters,int count)
+    {
+        char[,] alphabets = new char[,] {{'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+        'o','p','q','r','s','t','u','v','w','x','y','z'},
+        {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+        'T','U','V','W','X','Y','Z'} };
+
+        int[] indices = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                if (letters[i] == alphabets[0, j])
+                {
+                    indices[i] = j+1;
+                    break;
+                }
+
+                else if (letters[i] == alphabets[1, j])
+                {
+                    indices[i] = j+1;
+                    break;
+                }
+            }
+        }
+
+        for(int i = 0; i < count; i++)
+        {
+            Console.WriteLine("\nTHe Character {0} has an alphabet position of {1}",letters[i],indices[i]);
+        }
+    }
+}
